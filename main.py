36285fe561bed686d20cf99397221c3559cf36d0
Никита Асташкин <54978818+Svjashennik@users.noreply.github.com>
2019-10-25 11:40:37 +0300

    init commit
     azaza

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from multiprocessing import Process, Pool
+
+
+def forpotok(matrix1, matrix2, ind):
+    res = []
+    for k in range(0, len(matrix2[0])):
+        res.append(element((ind, k), matrix1, matrix2))
+    with open ('and.json', 'r') as file:
+        mat = json.load(file)
+        mat[ind] = res
+    with open('and.json', 'a') as file:):
+        
 
 
 def element(index, A, B):
@@ -10,7 +22,21 @@ def element(index, A, B):
     return res
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+if __name__ == '__main__':
+    matrix1 = [[2, 0, 1],
+               [1, 2, 5]]
+    matrix2 = [[1, 2],
+               [3, 4],
+               [3, 4]]
+    procs = []
+    an = [[] for i in range(len(matrix1))]
 
-print(element((1, 0), matrix1, matrix2))
+    for i in range(0, len(matrix1)):
+        proc = Process(target=forpotok, args=(matrix1, matrix2, i))
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
+
+    print(an)
