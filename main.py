3c6b44935093927dcd240c4ca58549a5f2f71ec9
polina pavlova <paulinapvlv@gmail.com>
2019-10-26 10:38:38 +0300

    Добавлена поэлементная запись матрицы в файл. Сделать поэлементную запись при получении нового элемента

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from multiprocessing import Pool,Process,Queue
 
-def element(index, m1, m2,q=0):
+def element(index, m1, m2,q = 0):
     """Поэлементное умножение матриц """
     i, j = index
     res = 0
@@ -13,22 +13,21 @@ def element(index, m1, m2,q=0):
     
 def mp_multiply(m1,m2):
     """Параллельное умножение матриц """
-    p=Pool()
-    q=Queue()
-    m3=[]
+    q = Queue()
+    m3 = []
     
     for i in range(len(m1)):
         for j in range(len(m1[0])):
-            p=Process(target=element,args=((i,j),m1,m2,q,))
+            p = Process(target = element,args = ((i,j),m1,m2,q,))
             p.start()
             m3.append(q.get())
             p.join()
     return m3
     
-def read_matrix(f,sep=";"):
+def read_matrix(f,sep = ";"):
     """Читает матрицу из файла с заданным разделителем """
-    strm=[] #матрица в строковом представлении
-    matrix=[] #матрица в целых числах
+    strm = [] #матрица в строковом представлении
+    matrix = [] #матрица в целых числах
     
     with open(f) as file_handler:
         for line in file_handler:#читаем строки из файла и добавляем в список
@@ -39,21 +38,20 @@ def read_matrix(f,sep=";"):
         
     for i in range(len(matrix)):#преобразуем значения в int
         for j in range(len(matrix[i])):
-            matrix[i][j]=int(matrix[i][j])
+            matrix[i][j] = int(matrix[i][j])
     return matrix
 
-def write_matrix(matrix,f="m.txt",sep=";",output=True):
+def write_matrix(matrix,f = "m.txt",sep = ";",mp = False,output = True):
     """Записывает матрицу в файл с заданным разделителем"""
-    strm=str(matrix)
-    mod=str()
-    
+    strm = str(matrix)
+    mod = str()
     try:
-        file=open(f)
+        file = open(f)
         file.close()
     except IOError as e:
-        mod="a+" #a+ создаёт файл, если он не существует
+        mod ="a+" #a+ создаёт файл, если он не существует
     else:
-        mod="w" #открывает файл на запись
+        mod = "w" #открывает файл на запись
     finally:
         with open(f,mod) as file_handler:
             for i in range(len(strm)):
@@ -61,21 +59,53 @@ def write_matrix(matrix,f="m.txt",sep=";",output=True):
         if output:
             print(matrix)
 
-def reshape(matrix,n,m):
+def element_write_matrix(num,index,size,f = "m.txt",sep = ";",output = True):
+    """Записывает матрицу в файл поэлементно """
+    mod = str()
+    i, j = index
+    n, m = size
+    exists= True if index != (0,0) else False 
+    print(exists)
+    try:
+        file = open(f)
+        file.close()
+    except IOError as e:
+        mod = "a" #a+ создаёт файл, если он не существует
+    else:
+        if not exists:
+            mod = "w" #открывает файл на запись (перезапись)
+        else:
+            mod="a"
+    finally:
+        with open(f,mod) as file_handler:
+            if j == m-1: #если последний элемент на строке, нет разделителя и новая строка
+                file_handler.write("{}{}".format(num,"\n")) #записывает числа, соединённые разделителем
+            else:
+                file_handler.write("{}{}".format(num,sep))
+            if output:
+                print(num)
+
+def reshape(matrix,size):
     """Преобразует одномерный массив в двумерный заданной размерности """
-    reshaped=[[0 for i in range(n)] for j in range(m)] #пустая матрица заданной размерности
-    if len(matrix)==n*m: 
+    n, m = size
+    reshaped = [[0 for i in range(n)] for j in range(m)] #пустая матрица заданной размерности
+    if len(matrix) == n*m: 
         for i in range(n):
            for j in range(m):
-               reshaped[i][j]=matrix[m*i+j]
+               reshaped[i][j] = matrix[m*i+j]
         return reshaped
     return -1 #возвращает, если размерность не соответствует заданной
 
-if __name__=="__main__":
+if __name__ == "__main__":
     m1=read_matrix("m1.txt") #i*j
     m2=read_matrix("m2.txt") #j*m 
     # результирующая матрица i*m
     print("Матрица 1:{} Матрица 2:{}".format(m1,m2))
-    write_matrix(reshape(mp_multiply(m1,m2),len(m1),len(m2[0])),"m3.txt")
-
+    print(reshape(mp_multiply(m1,m2),(len(m1),len(m2[0]))))
+    #m3=reshape(mp_multiply(m1,m2),(len(m1),len(m2[0])))
+    m3=reshape(mp_multiply(m1,m2),(len(m1),len(m2[0])))
+    element_write_matrix(m3[0][0],(0,0),(2,2),"m3.txt")
+    element_write_matrix(m3[0][1],(0,1),(2,2),"m3.txt")
+    element_write_matrix(m3[1][0],(1,0),(2,2),"m3.txt")
+    element_write_matrix(m3[1][1],(1,1),(2,2),"m3.txt")
             
