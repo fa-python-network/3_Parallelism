954fb6249313ba88123d7c5ef935c3aaab8fb0e3
Gamid <Gamid@mail.ru>
2019-10-28 01:56:52 +0300

    2,3 задания

--- multi.py
+++ multi.py
@@ -2,23 +2,32 @@ import os
 from multiprocessing import Pool
 
 c =[]
-
-n1 = int(input('Введите количество строк в 1 матрице: '))
-m1 = int(input('Введите количество столбцов в 1 матрице: '))
-n2 = int(input('Введите количество строк во 2 матрице: '))
-m2 = int(input('Введите количество столбцов во 2 матрице: '))
  
-mat1 = [[int(input('элементы 1 матрицы ')) for i in range(m1)] for j in range(n1)]
-mat2 = [[int(input('элементы 2 матрицы ')) for k in range(m2)] for l in range(n2)]
+mat1=[]
+mat2=[]
+with open ('matrix1.txt', 'r') as matrix1:
+	for i,line in enumerate(matrix1):
+		mat1.append([])
+		for k in (line.split()):
+				mat1[i].append(int(k))
 
-n = n1*m2
+with open ('matrix2.txt', 'r') as matrix2:
+	for i,line in enumerate(matrix2):
+		mat2.append([])
+		for k in (line.split()):
+				mat2[i].append(int(k))
+
+n1=len(mat1)
+
+n = n1*n1
+
 print(n)
 print(mat1)
 print(mat2)
 
 def q(k):
 	l=0
-	for i in range(m1):
+	for i in range(len(mat1[0])):
 		l+=mat1[k//n1][i]*mat2[i][k%n1]
 	print(l, os.getpid())
 	return(l)
@@ -39,3 +48,8 @@ for i in range(n1):
 		o[i].append(h[j+i*n1])
 
 print(o)
+
+with open ('matrix3.txt', 'w') as matrix3:
+	for i in o:
+		matrix3.write(' '.join(str(ch) for ch in i))
+		matrix3.write('\n')
