e48df6e297112875325629ef44da244ea1cbbbbc
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-10-29 01:02:41 +0300

    main tasks

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+from multiprocessing import Pool
+from random import randint
+import json
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+def mult(x, y, z, r):
+    result = (sum(i * j for i, j in zip(x, y)), z)
+    r.append(result)
+    return result
 
+def datarec(file, result):
+    with open(file, 'w') as f:
+        json.dump(result, f)
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+if __name__ =='__main__':
+    with open('data.json','r') as f:
+        matrices = json.load(f)
+        m1, m2 = matrices['m1'], matrices['m2']
+    print(*m1, '\n', *m2, sep='\n', end='\n\n')
 
-print(element((1, 0), matrix1, matrix2))
+    results = []
+    with Pool(5) as p:
+        results = p.starmap_async(mult, [(i, j, (ix * len(m2[0]) + jy + 1), results) for ix, i in enumerate(m1) for jy, j in enumerate(zip(*m2))]).get()
+
+    results.sort(key=lambda x: x[1])
+    results = [[*map(lambda x: x[0], results[i:i + len(m2[0])])] for i in range(0, len(results), len(m2[0]))]
+
+    print(*results, sep='\n')
+    datarec('result.json', results)
