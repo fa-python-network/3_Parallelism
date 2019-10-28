e6a5318262f8f978ab2d0b69ecedecb457e3402c
соня <sonyakt@bk.ru>
2019-10-29 03:43:30 +0500

    матрицы

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+from multiprocessing import Pool
+import json
+
+matrix = []
+
+with open('matrices.json', 'r') as f:
+    matrix = json.load(f)
+
+m1, m2 = matrix['m1'], matrix['m2']
+
+def mult(elem1, elem2, elem3, elem4):
+    result = (sum(i * j for i, j in zip(elem1, elem2)), elem3)
+    elem4.append(result)
+    return result
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+if __name__ == '__main__':
 
+    results = []
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+    with Pool(5) as p:
+        results = p.starmap_async(mult, [(i, j, (ix * len(m2[0]) + jy + 1), results) for ix, i in enumerate(m1) for jy, j in enumerate(zip(*m2))]).get()
 
-print(element((1, 0), matrix1, matrix2))
+    results.sort(key=lambda x: x[1])
+    results = [[*map(lambda x: x[0], results[i:i + len(m2[0])])] for i in range(0, len(results), len(m2[0]))]
+
+    with open('mult.json', 'w') as f:
+        json.dump(results, f)
