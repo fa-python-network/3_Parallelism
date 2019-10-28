31d8706072e7414e4067a5b07f1a7bed10f4022c
natashagushan <55361627+natashagushan@users.noreply.github.com>
2019-10-29 00:29:29 +0300

    matrix multiplication with file

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-from multiprocessing import  Pool
-from random import randint
+from multiprocessing import Pool
+import json
 
-def f(x,y) -> float:
-    rez = sum(i*j for i, j in zip(x,y))
-    dump(rez)
-    return rez  
 
-def dump(x):
-    global c
+def f(x, y, z, r) -> float:
+    result = (sum(i * j for i, j in zip(x, y)), z)
+    r.append(result)
+    return result
 
-    with open('file','a') as f:
-        if c % 4 == 0:
-            f.write(f'{x}\n')
-        else:
-            f.write(f'{x} ')
-            
-    c += 1
-            
-        
-c = 1
-if __name__ =='__main__':
-    m1 = [[randint(-5,5) for i in range(3)] for i in range(4)]
-    m2 = [[randint(-5, 5) for i in range(4)] for i in range(3)]
-    print(*m1,'\n' , *m2, sep = '\n', end = '\n\n')
 
-    print([(i,j) for i in m1 for j in zip(*m2)], end = '\n\n')
+if __name__ == '__main__':
+    with open('entrance.json', 'r') as fl:
+        matrices = json.load(fl)
+        m1, m2 = matrices['first'], matrices['second']
+
+    print(*m1, '\n', *m2, sep='\n', end='\n\n')
+
+    columns, rows = len(m1[0]), len(m2)
+    if columns != rows:
+        raise ValueError('Columns != Rows')
+
+    second_columns = len(m2[0])
+
+    results = []
+
+
     with Pool(5) as p:
-        results = p.starmap_async(f, [(i,j) for i in m1 for j in zip(*m2)]).get()
-    print(results, '\n')
-    new_m = [results[i:i+4] for i in range(0, len(results), 4)]
-    print(*new_m, sep = '\n')
+        results = p.starmap_async(f,
+                                  [(i, j, (ix * second_columns + jy + 1), results) for ix, i in enumerate(m1) for jy, j
+                                   in enumerate(zip(*m2))]).get()
 
+    results.sort(key=lambda x: x[1])
+    results = [[*map(lambda x: x[0], results[i:i + second_columns])] for i in range(0, len(results), second_columns)]
 
+    print(*results, sep='\n')
+
+    with open('exitt.json', 'w') as fl:
+        json.dump(results, fl)
