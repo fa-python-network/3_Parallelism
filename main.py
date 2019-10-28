9b0d0bdd3a5ad2469309293ea86f1161e523f835
StepIgor <igor.stepanov@difres.ru>
2019-10-28 19:48:56 +0300

    Перемножение по строкам в разных процессах

--- main.py
+++ main.py
@@ -1,33 +1,55 @@
-def multiply_matrix(m1,m2):
+from multiprocessing import Process, Pool, Array
 
-	m3 = []
+
+def multiply_matrix(m1,m2,linenum,matrix_result):
+
 	temp = []
 	sum = 0
 	
-	if len(m1) == len(m2[0]) or len(m1[0]) == len(m2):
+	if len(m2) != len(m1[0]):
+		print('error! incorrent input!')
+		return "error!"
+	else:
 		cols1 = len(m1[0])
 		lines1 = len(m1)
 		cols2 = len(m2[0])
 		lines2 = len(m2)
 		
-		for line1 in range(0,lines1):
+		for line1 in range(linenum,linenum+1):
 			for col2 in range(0,cols2):
 				for col1 in range(0,cols1):
 					sum+=m1[line1][col1]*m2[col1][col2]
 				temp.append(sum)
 				sum = 0
-			m3.append(temp)
+			for elem in range(0,len(temp)):
+				matrix_result[linenum*len(temp)+elem] = temp[elem]
 			temp = []
-			
-	else:
-		return "error!"
-		
-		
-	return m3
 	
 #start data point
 
-matrix1 = [[1,2,3],[1,2,3],[1,2,3]]
-matrix2 = [[3,3,3],[3,3,3],[2,3,3]]
+matrix1 = [[4,4,4],[5,5,5]]
+matrix2 = [[4,4,4],[5,5,5],[6,6,6]]
+validmatrix = matrix2
+if len(matrix1) < len(matrix2):
+	validmatrix = matrix1
 
-print(multiply_matrix(matrix1,matrix2))
+matrix_result = Array('i',[0 for i in range(0,len(validmatrix)*len(validmatrix[0]))])
+
+
+if __name__ == '__main__':
+	for line in range(0,len(matrix1)):
+		p1 = Process(target=multiply_matrix,args=(matrix1,matrix2,line,matrix_result))
+		p1.start()
+		p1.join()
+
+
+	#print result
+	now = 0
+	for line in range(0,len(validmatrix)):
+		print('[',end='')
+		for elem in range(0,len(validmatrix[line])-1):
+			print(f'{matrix_result[now]},',end='')
+			now+=1
+		print(f'{matrix_result[now]}',end='')
+		now+=1
+		print(']\n',end='')
