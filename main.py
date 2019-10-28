8ff5fb3bf3410958410591aa2f6bbc2578a3fee8
StepIgor <igor.stepanov@difres.ru>
2019-10-28 18:18:58 +0300

    Правильная работа функции получена!

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-from multiprocessing import Process
+def multiply_matrix(m1,m2):
 
-
-def elemresult(index, A, B, res):
-    glodal res
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+	m3 = []
+	temp = []
+	sum = 0
+	
+	if len(m1) == len(m2[0]) or len(m1[0]) == len(m2):
+		cols1 = len(m1[0])
+		lines1 = len(m1)
+		cols2 = len(m2[0])
+		lines2 = len(m2)
+		
+		for line1 in range(0,lines1):
+			for col2 in range(0,cols2):
+				for col1 in range(0,cols1):
+					sum+=m1[line1][col1]*m2[col1][col2]
+				temp.append(sum)
+				sum = 0
+			m3.append(temp)
+			temp = []
+			
+	else:
+		return "error!"
+		
+		
+	return m3
 	
 #start data point
 
 matrix1 = [[1,2,3],[1,2,3],[1,2,3]]
-matrix2 = [[3,3],[3,3],[3,3]]
-matrixresult = []
-	
-#convert result matrix with correct length
-	
-for smth in range(len(matrix1)):
-		matrixresult.append([])
-		
-for line in range(len(matrixresult)):
-	for smth in range(len(matrix2[0])):
-		matrixresult[line].append(0)
-		
-#call function
+matrix2 = [[3,3,3],[3,3,3],[2,3,3]]
 
-if len(matrix1) == len(matrix2[0]) or len(matrix1[0]) == len(matrix2):
-	for line in range(len(matrix1)):
-				for elem in range(len(matrix1[0])):
-					elemresult((line,elem),matrix1,matrix2)
-
-	#print result
-	print(matrixresult)
-else:
-	print('invalid input data')
+print(multiply_matrix(matrix1,matrix2))
