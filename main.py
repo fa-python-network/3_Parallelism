2933c9536e0f78a71e5ccea2dc8205a38fd14e33
dadingdong <55705491+dadingdong@users.noreply.github.com>
2019-10-28 19:07:19 +0300

    final
    main.py -- основной файл

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
+from multiprocessing import Pool
+from random import randint
+import json
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+def f(x, y, z, r) -> float:
+    result = (sum(i * j for i, j in zip(x, y)), z)
+    r.append(result)
+    return result
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+if __name__ == '__main__':
+    with open('in.json', 'r') as fl:
+        matrices = json.load(fl)
+        m1, m2 = matrices['first'], matrices['second']
+    
+    #m1 = [[randint(-5, 5) for i in range(3)] for j in range(4)]
+    #m2 = [[randint(-5, 5) for i in range(4)] for j in range(3)]
+    print(*m1, '\n', *m2, sep='\n', end='\n\n')
 
-print(element((1, 0), matrix1, matrix2))
+    columns, rows = len(m1[0]), len(m2)
+    if columns != rows:
+        raise ValueError('Columns != Rows')
+
+    second_columns = len(m2[0])
+
+    results = []
+
+    #print([(i, j, (ix, jy)) for ix, i in enumerate(m1) for jy, j in enumerate(zip(*m2))], end='\n\n')
+
+    with Pool(5) as p:
+        results = p.starmap_async(f, [(i, j, (ix * second_columns + jy + 1), results) for ix, i in enumerate(m1) for jy, j in enumerate(zip(*m2))]).get()
+    
+    results.sort(key=lambda x: x[1])
+    results = [[*map(lambda x: x[0], results[i:i + second_columns])] for i in range(0, len(results), second_columns)]
+
+    print(*results, sep='\n')
+    
+    with open('out.json', 'w') as fl:
+        json.dump(results, fl)
