db41a4cff6433e47a3ba9360ca1887bddb6f321d
nastya <pin-2169@rambler.ru>
2019-10-28 17:58:55 +0300

    Matrix multiplication with randint

--- matrixmult.py
+++ matrixmult.py
@@ -1,12 +1,13 @@
 from multiprocessing import  Pool
+from random import randint
 
 def matrixmult(a, b):
     res_mul = sum(i*j for i, j in zip(a,b))
     return res_mul  
 
 if __name__ =='__main__':
-    matrix1=[[1,2,3], [4,5,6]]
-    matrix2=[[7,8], [9, 10], [11, 12]]
+    matrix1 = [[randint(-10,10) for i in range(2)] for i in range(3)]
+    matrix2 = [[randint(-10, 10) for i in range(3)] for i in range(2)]
 
     with Pool(2) as pool:
     #умножаем каждую строку 1-ой матрицы((1,2,3) и (4,5,6))
