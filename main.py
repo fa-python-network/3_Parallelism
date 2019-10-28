782611c6907704417a94706d4caece46884e20dd
Nick <niki150700@yandex.ru>
2019-10-28 21:55:50 +0000

    Matrix

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+from multiprocessing import Process, Pool
 
+def element(*args):
+	elemen = args[0]
+	index = list(args[1])	
+	result = (sum(i*j for i,j in zip(elemen, index)))
+	return result
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+matrix1 = [[1, 1, 2] , [2, 2, 1], [3, 3, 3]]
+matrix2 = [[2, 3,1] , [3, 1, 4], [0,3,3]]
+print('Первая матрица\n', matrix1)
+print('\nВторая матрица\n', matrix2)
 
+C1 = matrix1
+C2 = list(zip(*matrix2))
+result = []
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
-
-print(element((1, 0), matrix1, matrix2))
+if __name__ == '__main__':
+	pool = Pool()
+	result = pool.starmap(element, [(i,j) for i in C1 for j in C2])
+	res = [result[i:i+len(C1[0])] for i in range(0, len(result), len(C1[0]))]
+	print('\nКонечная матрица\n')
+	for i in range(len(res)):
+		print(res[i])
