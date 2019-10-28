1d9e2d712eaa9daa260aa46e29eb73569a2ae956
Konstantin <48581717+BondarevKonstantin@users.noreply.github.com>
2019-10-28 21:33:37 +0300

    Bondarevpi18-2
    ou

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
+from multiprocessing import Pool
+import json
+import numpy as np
 
 
-def element(index, A, B):
+
+def element(args):
+    index, A, B = args[0], args[1], args[2]
     i, j = index
     res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
+    N = A.shape[1] or B.shape[0]
     for k in range(N):
-        res += A[i][k] * B[k][j]
+        res += A[i, k] * B[k, j]
     return res
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+def generate_map(matrix1, matrix2):
+    args = []
+    for i in range(matrix1.shape[0]):
+        for j in range(matrix2.shape[1]):
+            args.append(((i, j), matrix1, matrix2))
+    return args
 
-print(element((1, 0), matrix1, matrix2))
+
+def to_file(matrix, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        for line in matrix:
+            for digit in line:
+                f.write(f'{digit}\t')
+            f.write('\n')
+
+
+if __name__ == "__main__":
+    with open('matrix1.json', encoding='utf-8') as f:
+        matrix1 = np.matrix(json.load(f))
+
+    with open('matrix2.json', encoding='utf-8') as f:
+        matrix2 = np.matrix(json.load(f))
+
+    p = Pool(matrix1.shape[0] * matrix2.shape[1])
+    args = generate_map(matrix1, matrix2)
+    result = np.array(p.map(element, args))
+    matrix = result.reshape(matrix1.shape[0], matrix2.shape[1])
+    print(matrix)
+    to_file(matrix, 'result.txt')
