f40557f56328e6530a93f59565461331b36e60d9
root <vmolchanovag@mail.ru>
2019-10-29 18:00:51 +0300

    перемножение матриц

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+from multiprocessing import Pool
 
+def el(*args):
+	m=args[0]
+	ind = list(args[1])
+	res=sum(i*j for i,j in zip(m,ind))
+	return res
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+m1=[[1,1],
+	[2,2]]
 
+m2=[[3,4],
+	[3,4]]
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+m22=list(zip(*m2))
 
-print(element((1, 0), matrix1, matrix2))
+res=[]
+M=[]
+pool=Pool(3)
+for i in range(len(m1[0])):
+	for j in range(len(m22)):
+		res = pool.starmap(el, [(i,j) for i in m1 for j in m22])
+	
+print(res)
