2b88520ac54d7fb39de0e3354d81edfca2435735
Сергей Ефремов <yefr@MacBook-Pro-Sergej-3.local>
2019-10-29 18:13:03 +0300

    Random matrices and writing into file

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from multiprocessing import  Pool
+from multiprocessing import Pool
+from random import randint
+
 
 def matrix_multiplier(a, b):
+    s = sum(i * j for i, j in zip(a, b))
+    writing_into_file(s)
+    return s
 
-     s = sum(i*j for i, j in zip(a,b))
-     return s
 
-if __name__ =='__main__':
-    matrix_a=[[1,1], [2,2]]
-    matrix_b=[[12, 21], [1, 11], [5, 2]]
+def writing_into_file(a):
+    global matrix_b, quantity
+    with open('matrix_multiplier.txt', 'a') as file:
+
+        if quantity % (len(matrix_b[0])) == 0:
+            file.write(f'{a}\n')
+
+        else:
+            file.write(f'{a} ')
+    quantity += 1
+
+quantity = 1
+
+if __name__ == '__main__':
+    matrix_a = [[randint(-50, 50) for i in range(2)] for i in range(2)]
+    matrix_b = [[randint(-50, 50) for i in range(2)] for i in range(2)]
 
     with Pool(2) as pool:
-        mat = pool.starmap(matrix_multiplier, [(i,j) for i in matrix_a for j in zip(*matrix_b)])
+
+        mat = pool.starmap(matrix_multiplier, [(i, j) for i in matrix_a for j in zip(*matrix_b)])
     print(mat)
 
-    result = [mat[i:i+len(matrix_b[0])] for i in range(0, len(mat), len(matrix_b[0]))]
-    print("Произведение исходных матриц = ", result)
+    result = [mat[i:i + len(matrix_b[0])] for i in range(0, len(mat), len(matrix_b[0]))]
+    print("Произведение матриц = ", result)
