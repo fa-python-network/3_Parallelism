11a25a5814532f05ba43d30128d9e5477b9fb935
vlack <kirilkin12@gmail.com>
2019-10-29 17:04:12 +0300

    Блочное кодирование fixed

--- encoding.py
+++ encoding.py
@@ -1,21 +1,26 @@
 from multiprocessing import Pool, cpu_count
 
-FILENAME = 'lorem.enc'
+CODE = b'qwertyuiop[]asdf'
 
 
-def encoder(b: bytes, code: bytes = b'qwertyuiop[]asdf') -> bytes:
-    return bytes(a ^ b for a, b in zip(b, code))
+def encoder(b: bytes) -> bytes:
+    return bytes(a ^ b for a, b in zip(b, CODE))
 
 
 if __name__ == '__main__':
-    with open(FILENAME, 'rb') as file:
+    filename = input('Input filename ')
+    CODE = input('Input 16-symbols code phrase ')
+    while len(CODE) != 16:
+        CODE = input('Input 16-symbols code phrase ')
+    CODE = CODE.encode()
+    with open(filename, 'rb') as file:
         to_encode = file.read()
-    print(len(to_encode) % 16)
-    to_encode += b' ' * (len(to_encode) % 16)
+    to_add = 16 - (len(to_encode) % 16)
+    to_encode += b' ' * (to_add if to_add != 16 else 0)
 
     processes_pool = Pool(cpu_count())
     blocks = [to_encode[i:i + 16] for i in range(0, len(to_encode), 16)]
 
     encoded = processes_pool.map(encoder, blocks)
-    with open(FILENAME + '.enc', 'wb')as file:
+    with open(filename + '.enc', 'wb')as file:
         file.write(b''.join(encoded))
