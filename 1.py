4c8ed06243201ee8d34c473601dc175252bd196d
Shalyapin2008 <55088406+Shalyapin2008@users.noreply.github.com>
2019-10-29 17:14:15 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,11 +1,11 @@
 
-from multiprocessing import Process, Pool
+from multiprocessing import Process, Pool, Queue
 
 
 
 
 	
-    
+q=Queue()    
 def main():
 	res=0
 	A=[[1,2,3],[4,5,6]]
@@ -16,15 +16,17 @@ def main():
 	procs=[]
 	indexes=[]
 
+	#q=Queue()
+	
 	if len(A[0])!=len(B):
 		print("Матртицы не согласованы")
-	#print("перед елсе С=",C)
+	
 	else:
 		 							#размер итоговой матрицы
 		size.append(len(A))		
 		size.append(len(B[0]))			
 		
-		for i in range(size[0]):
+		for i in range(size[0]):			#создание пустой матрицы
 			C.append([])
 			for n in range(size[1]):
 				C[i].append(".")
@@ -39,21 +41,41 @@ def main():
 		
 
 
-		for i in range(0,len(indexes)):
+		for i in range(0,len(indexes)):  #запуск процессов
 			
-			proc = Process(target=jeff, args=(indexes[i][0],indexes[i][1],A,B,C,res))
+			proc = Process(target=jeff, args=(indexes[i][0],indexes[i][1],A,B,C,res,q))
 			procs.append(proc)
 			proc.start()
 			
 		for proc in procs:
 			proc.join()
-
-
+		
+		data2=[]
+		count=0
+		
+		while count!=4:
+			count+=1
+			
+			data2.append(q.get())
+			
+			
+				
+		print("!!@")
+		print(data2)
+		q.close()
+		
+		for i in range (len(indexes)):
+			C[data2[i][0]][data2[i][1]]=data2[i][2]
+			
+			
+		print("---"*10)	
 		for i in C:
 			print(i)
+		
+			
 
 
-def jeff(i, j, A, B,C, res):
+def jeff(i, j, A, B,C, res,q):
 	  
 	res=0
 	N = len(A[0]) or len(B)
@@ -61,10 +83,9 @@ def jeff(i, j, A, B,C, res):
 		
 		res += A[i][k] * B[k][j]
 	C[i][j]=res
-	print("res=",res)
-	print(C[i][j])
+	q.put([i,j,res])
 	print(C)
-
+	
 	return res
 
 	
