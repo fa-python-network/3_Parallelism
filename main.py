06cb6ec2da24f6ecacf67c094c7cc90ff8357e11
Gamid <Gamid@mail.ru>
2019-10-30 15:11:18 +0300

    Доп 2

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
+"""
+Процесс перемножения завершается нажатием Ctrl C, вместе с ним убивается процесс генерации матриц.
+Работает криво, но работает
+А первый номер тем способом с помощью таймера решать долго:(
+"""
+
+from multiprocessing import Process, Queue, Pool
+import random
+from functools import partial
+from itertools import product
+import sys, signal
+
+def signal_handler(signal, frame):
+	global sig
+	sig=1
+	
+
+def creator(r,q):
+	while True:
+	
+		m=[[random.randint(-10,10) for i in range(r)] for j in range(r)]
+		q.put(m)
+		
+sig=0
+
+if __name__=='__main__':
+	q=Queue()
+	n1=int(input('Razmernost: '))
+	process_one = Process(target=creator,args=(n1,q))
+	process_one.start()
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
 
+	def my_(k):
+		l=0
+		for i in range(n1):
+			l+=mat1[k//n1][i]*mat2[i][k%n1]
+		return(l)
+		
+	while True:
+		mat1=q.get()
+		mat2=q.get()
+		c=[]
+		for i in range(n1*n1):
+			c.append(i)
+		pool = Pool(processes=5)
+		h=(pool.map(my_,c))
+		o=[]
+		for i in range(n1):
+			o.append([])
+			for j in range(n1):
+				o[i].append(h[j+i*n1])
+		print('REZ',o)
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+		signal.signal(signal.SIGINT,signal_handler)
+		if sig==1:
+			process_one.terminate()
+			break
 
-print(element((1, 0), matrix1, matrix2))
+	q.close()
+	q.join_thread()
+	process_one.join()
+
+	print("VSE")
