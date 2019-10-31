98b16e878f3b9b3e3b1694e5765a22c8331d47ad
polina pavlova <paulinapvlv@gmail.com>
2019-10-31 19:30:38 +0300

    Починено умножение неквадратных матриц

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ def element(index, m1, m2,q = 0):
         res += m1[i][k] * m2[k][j]
     if not q:
         return res
-    return q.put(res) #реализация очереди
+    return q.put(res) #реализация очереди 
     
-def mp_multiply(m1,m2,mp_write = False,file = "",sep=";",generate=False):
+def mp_multiply(m1,m2,mp_write = False,file = "",sep=";",generate = False):
     """Параллельное умножение матриц """
     q = Queue()
     m3 = []
     randomized=[False for i in range(len(m1[0])) ] #список булевых значений, отражающий, был ли сгенерирован данный столбец случайных чисел
+    
     for i in range(len(m1)):
         if generate: #если генерируем случайную матрицу           
                 randomize_row(m1,i) 
-        for j in range(len(m1[0])):
+        for j in range(len(m2[0])):
             if generate:
                 if not randomized[j]:#благодаря этому уже сгенерированные столбцы 
                     randomize_column(m2,j) #не заменяются новыми значениями
                     randomized[j]=True
             p = Process(target = element,args = ((i,j),m1,m2,q,))
             p.start()
-            res=q.get()
-            if mp_write:
+            res = q.get()
+            if mp_write: #если запись в файл
                 element_write_matrix(res,(i,j),(len(m1),len(m2[0])),file,sep)
             m3.append(res)
             p.join()
@@ -56,20 +57,15 @@ def generate_empty_square(size):
     """Генерирует пустую квадратную матрицу """
     return [[0 for i in range(size)] for j in range(size)]
 
-def randomize_row(m,row,lower_bound=-10,upper_bound=10):
+def randomize_row(m,row,lower_bound=-10,upper_bound = 10):
     """ Рандомизирует row строку матрицы"""
-    t=list()
     for n in range(len(m[0])): 
-        t.append(random.randint(lower_bound,upper_bound))
-    m[row]=t
-
-def randomize_column(m,column,lower_bound=-10,upper_bound=10):
+        m[row][n]=random.randint(lower_bound,upper_bound)
+    
+def randomize_column(m,column,lower_bound=-10,upper_bound = 10):
     """ Рандомизирует column столбец матрицы"""
-    t=list()
     for n in range(len(m)):
-        t.append(random.randint(lower_bound,upper_bound))
-    for n in range(len(m)):
-        m[n][column]=t[n]
+        m[n][column]=random.randint(lower_bound,upper_bound)
 
 def write_matrix(matrix,f = "m.txt",sep = ";"):
     """Записывает матрицу в файл с заданным разделителем"""
@@ -90,14 +86,14 @@ def write_matrix(matrix,f = "m.txt",sep = ";"):
 def element_write_matrix(num,index,size,f = "m.txt",sep = ";"):
     """Записывает матрицу в файл поэлементно """
     mod = str()
-    i, j = index
-    n, m = size
+    i, j = index #текущие индексы
+    n, m = size #размер матрицы
     exists= True if index != (0,0) else False #проверка на то, существуют ли элементы до этого
     try:
         file = open(f)
         file.close()
     except IOError as e:
-        mod = "a" #a+ создаёт файл, если он не существует
+        mod = "a" #a создаёт файл, если он не существует
     else:
         if not exists:
             mod = "w" #открывает файл на запись (перезапись)
@@ -123,7 +119,7 @@ def reshape(matrix,size):
 
 def output(m):
     """ Красивый вывод матрицы"""
-    string=str()
+    string = str()
     for i in range(len(m)):
         for j in range(len(m[0])):
             string+="{:4d} ".format(m[i][j])
@@ -131,20 +127,20 @@ def output(m):
     return string
 
 if __name__ == "__main__":
-    m1=read_matrix("m1.txt") #i*j
-    m2=read_matrix("m2.txt") #j*m 
+    m1 = read_matrix("m1.txt") #i*j
+    m2 = read_matrix("m2.txt") #j*m 
     print(m1,m2)
     # результирующая матрица i*m
     
-    m3=reshape(mp_multiply(m1,m2,mp_write=True,file="m3.txt"),(len(m1),len(m2[0])))
+    m3 = reshape(mp_multiply(m1,m2,mp_write = True,file="m3.txt"),(len(m1),len(m2[0])))
 
     print(m3)
     #print("Матрица 1:\n{}\n Матрица 2:\n{}\n Матрица 3:\n {}\n".format(output(m1),output(m2),output(m3)))
     
-    n=int(input("Введите размерность квадратной матрицы случайных чисел: "))
-    m4=generate_empty_square(n) #пустые квадратные матрицы для 
-    m5=generate_empty_square(n)
-    m6=reshape(mp_multiply(m4,m5,generate=True),(len(m4),len(m4)))
+    n = int(input("Введите размерность квадратной матрицы случайных чисел: "))
+    m4 = generate_empty_square(n) #пустые квадратные матрицы для 
+    m5 = generate_empty_square(n)
+    m6 = reshape(mp_multiply(m4,m5,generate = True),(len(m4),len(m4)))
     print("Матрица 4:\n {}\n Матрица 5:\n {} \nМатрица 6:\n {} ".format(output(m4),
           output(m5),output(m6)))
     
