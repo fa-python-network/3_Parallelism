526b6b32e58edcd2223abd33b33fdba572879bb2
Nick <npk001@mail.ru>
2019-10-31 23:21:22 +0300

    Основное задание сделано (кроме промежуточных выводов). 1 задание допа также.

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
+from multiprocessing import Process, Pool
 
-
-def element(index, A, B):
-    i, j = index
+def element(*args):
+    i, j = args[0]
+    A = args[1]
+    B = args[2]
     res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
+    for k in range(len(A[i])):
+        res += int(A[i][k]) * int(B[k][j])
     return res
 
+def ch_mtrx(A, B):
+    try:
+        for i in range(len(A)):
+            assert len(A[i])==len(B)                
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+        for i in range(1, len(A[0])):
+            assert len(B[i])==len(B[i-1])
+    except AssertionError:
+        print("Wrong matrix")
+        exit(0)
+        
+def extract(f):
+    mtrx = []
+    for line in f:
+        mtrx.append(list(line.split()))
+    return mtrx
 
-print(element((1, 0), matrix1, matrix2))
+def involve(f, C, ln, st):
+    s = ""
+    for i in range(ln):
+        s = ""
+        for j in range(st):
+            s += str(C[i+j]) + " "
+        f.write(s + "\n")
+    
+        
+f = open("m1.txt")
+mtrx1 = extract(f)
+f = open("m2.txt")
+mtrx2 = extract(f)
+ch_mtrx(mtrx1, mtrx2)
+
+ln = len(mtrx2[0])
+st = len(mtrx1)
+
+mtrx3 = []
+
+if __name__ == '__main__':
+    __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
+    with Pool(ln*st) as p:
+        mtrx3 = (p.starmap(element, [((i,j), mtrx1, mtrx2) for i in range(ln) for j in range(st) ] ))
+        
+    f = open("m3.txt", "w")    
+    involve(f, mtrx3, ln, st)
+    f.close()         
