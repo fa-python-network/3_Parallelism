25ebf05bbb32ee6b42226f84b55b81d5c2dd7bef
Sangadzhi <zaiasg@mail.ru>
2019-11-01 02:03:19 +0300

    add

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
-from multiprocessing import Pool
+from multiprocessing import Process,Pool, Manager
+from random import randint
 
-def element(*args, **kwargs):
-    elem = args[0]
-    index = list(args[1])	
-    res = (sum(i*j for i,j in zip(elem, index)))
+def forpotok(matrix1, matrix2, ind, All):
+    res = All.an
+
+    for  k in range(0, len(matrix2[0])):
+        res[ind].append(element((ind, k), matrix1, matrix2))
+    All.an = res
+
+
+def check_matr(All):
+    while All.lst_mat:
+        procs = []
+        lst = All.lst_mat
+        matrix1, matrix2 = lst.pop()
+        All.lst_mat = lst
+        All.an = [[] for i in range(len(matrix1))]
+        for i in range(0, len(matrix1)):
+            proc = Process(target=forpotok, args=(matrix1, matrix2, i, All))
+            procs.append(proc)
+            proc.start()
+        for proc in procs:
+            proc.join()
+        print(All.an)
+        if All.com == "exit":
+            break
+
+
+def matrix_gen(All):
+    while True:
+        if All.com == "exit":
+            break
+        stro1 = randint(2, 5)
+        stol1 = randint(2, 5)
+        stro2 = randint(2, 5)
+        stol2 = stro1
+        matrix1 = [[randint(-5, 5) for i in range(stro1)] for i in range(stol1)]
+        matrix2 = [[randint(-5, 5) for i in range(stro2)] for i in range(stol2)]
+        lst = All.lst_mat
+        lst.append((matrix1,matrix2))
+        All.lst_mat = lst
+
+
+
+def element(index, A, B):
+    i, j = index
+    res = 0
+    # get a middle dimension
+    N = len(A[0]) or len(B)
+    for k in range(N):
+        res += A[i][k] * B[k][j]
     return res
 
-def dop1(m1,m2):
-    x = len(m1[0])
-    y = len(m2)
-    return x*y 
-
-def matrix_search(file):
-    res = []
-    with open(file,"r") as f:
-        for elem in f:
-            res.append(list(map(int, elem.split())))
-    return res
-
-def output(res):
-    with open("result_multiply.txt","w") as f:
-        for lines in res:
-            print(" ".join(str(i) for i in lines), file=f)
-
-
-m1 = matrix_search("matrix1.txt")
-
-m2 = matrix_search("matrix2.txt")
-
-print(' First matrix ', m1)
-print(' Second matrix ', m2)
-print(' Second modificated matrix ', list(zip(*m2)))
-result = []
 
 if __name__ == '__main__':
-    pool = Pool(processes=dop1(m1,m2))
-    multiplues = pool.starmap(element, [(i,j) for i in m1 for j in list(zip(*m2))]) #m2 modificated
-    result = [multiplues[i:i+len(m1[0])] for i in range(0, len(multiplues), len(m1[0]))]
-    output(result)
-    print(' Final matrix ')
-    for i in range(len(result)):
-        print(' '.join(map(str, result[i])))
+    mn = Manager()
+    All = mn.Namespace()
+    All.lst_mat = []
+    All.com = ''
+    genproc =Process(target=matrix_gen, args=([All]))
+    genproc.start()
+    matproc =Process(target=check_matr, args=([All]))
+    matproc.start()
+    while All.com != "exit":
+        All.com = input()
+    matproc.join()
+
+
+#(exit для завершения)
+   
