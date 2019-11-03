544533b27e81daf0541a1ed1e2ec52927816ce73
polina pavlova <paulinapvlv@gmail.com>
2019-11-03 19:45:24 +0300

    Теперь можно шифровать сообщения с кириллицей

--- cipher.py
+++ cipher.py
@@ -7,35 +7,31 @@ Created on Fri Nov  1 08:59:51 2019
 
 import random
 
-def generate_key(length):
-    global chars
+def generate_key(length,ch):
+    """Сгенерировать ключ данной длины рандомно из списка данных символов """
     key=str()
-    chars=[chr(i) for i in range(32,128)]
     for i in range(length):
-        key+=random.choice(chars)
+        key+=random.choice(ch)
     return key
 
-def encrypt(message,key):
-    global chars
+def encrypt(message,key,ch):
+    """Зашифровать сообщение данным ключом, оперирируя заданным списком символов. 
+    Символы ключа, сгенерированные generate_key, должны быть подмножеством ch!!!!"""
     encrypted=str()
     for i in range(len(message)):
-        index=chars.index(message[i])
-        shift=chars.index(key[i])
-        encrypted+=chars[(index+shift)%len(chars)]
+        index=ch.index(message[i])
+        shift=ch.index(key[i])
+        encrypted+=ch[(index+shift)%len(ch)]
     return encrypted
 
-def decrypt(message,key):
-    global chars
-    decrypted=str()
-    for i in range(len(message)):
-        index=chars.index(message[i])
-        shift=chars.index(key[i])
-        decrypted+=chars[(index-shift)]
-    return decrypted
 
-message="ashfgegfr"
-key=generate_key(len(message))
-print(key)
-encypted=encrypt(message,key)
-print(decrypt(message,key))
-#print(encrypt("dgdshdhe",key))
+key=str()
+ecnrypted=str()
+symbols=[chr(i) for i in range(32,65)]
+latinchars=[chr(i) for i in range(65,128)]
+russianchars=[chr(i) for i in range(1040,1104)]
+chars=symbols+latinchars+russianchars
+message=input("Введите сообщение ")
+key=generate_key(len(message),russianchars)
+encrypted=encrypt(message,key,symbols+russianchars)
+print(key+"\n"+encrypted)
