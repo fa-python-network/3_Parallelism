c778330e30e4751dba0cc0ed92dcf11fadef8d9f
nastyapl77 <55870686+nastyapl77@users.noreply.github.com>
2019-11-06 22:22:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
+def compute_matrix_element(args):
+    s1, s2, filename = args
+
+    elem = 0
+    for i in range(len(s1)):
+        elem += s1[i] * s2[i]
+
+    with open(filename, 'w') as f:
+        f.write(str(elem))
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+def multiply_matrices(m1, m2, filename='result.txt'):
+    from multiprocessing import Pool
+    from os import remove
+
+    m2_columns = []
+    for i in range(len(m2[0])):
+        m2_columns.append([st[i] for st in m2])
+
+    for st1 in m1:
+        assert len(st1) == len(m2), 'Кол-во столбцов первой матрицы не равно кол-ву строк второй.'
+
+    pool = Pool(processes=8)  # чаще всего 4 ядра, а из-за hyper-threading - 4*2
+    elem_filenames = []
+    for st_index, st in enumerate(m1):
+        loc_filenames = [f'element_{chr(ord("a") + col_index)}{st_index}' for col_index in range(len(m2_columns))]
+        elem_filenames.extend(loc_filenames)
+        pool.map(compute_matrix_element, zip([st] * len(m2_columns), m2_columns, loc_filenames))
+
+    with open(filename, 'w') as res_file:
+        for loop in range(len(m1)):
+            for col_index in range(len(m2_columns)):
+                elem_filename_index = loop * len(m2_columns) + col_index
+                with open(elem_filenames[elem_filename_index]) as elem_file:
+                    res_file.write(elem_file.read() + (' ' if col_index != len(m2_columns) - 1 else ''))
+                remove(elem_filenames[elem_filename_index])
+            res_file.write('\n')
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+def read_matrix(filename):
+    with open(filename) as f:
+        result = []
+        line = f.readline().split()
+        normal_len = len(line)
 
-print(element((1, 0), matrix1, matrix2))
+        while line:
+            if len(line) != normal_len:
+                raise ValueError('Строки матрицы имеют разную длину')
+
+            for i in range(len(line)):
+                line[i] = int(line[i])
+            result.append(line)
+
+            line = f.readline().split()
+
+    return result
+
+
+if __name__ == '__main__':
+    m1, m2 = read_matrix('m1.txt'), read_matrix('m2.txt')
+    multiply_matrices(m1, m2)
