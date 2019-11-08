252602b5b51fd9cc18ddd534fb81e1c3a4c006cc
ilolivie <54978914+ilolivie@users.noreply.github.com>
2019-11-08 20:25:52 +0300

    основное задание матрицы

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
+from multiprocessing import Pool
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+def element(lst1, lst2, number, index):
+    n = len(lst1)
+    value = 0
+    for i in range(n):
+        value += lst1[number][i] * lst2[i][index]
+    return value
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+def read(file):
+    m3 = []
+    with open(file, 'r') as f:
+        for line in f:
+            m3.append(list(map(int, line.split())))
+    return m3
 
-print(element((1, 0), matrix1, matrix2))
+
+def write(m3):
+    open('new.txt', 'w').close()    
+    with open('new.txt', 'a') as f:
+        for line in m3:
+            print(" ".join(str(x) for x in line), file=f) 
+
+
+a = read('matrix1.txt')
+b = read('matrix2.txt')
+
+n = len(a)
+p = []
+for i in range(n):
+    for j in range(n):
+        p.append([i, j])
+p = [[a,b,i[0],i[1]]for i in p]
+pool = Pool(len(p))
+p = pool.starmap(element, p)
+
+lst = []
+new = []
+k = 0
+for i in p:
+    k += 1
+    if k%n != 0:
+        lst.append(i)
+    else:
+        lst.append(i)
+        new.append(lst)
+        lst = []
+print(new)
+write(new)
