8b9255cf280881bb0c365f1de1350d144f291e11
RadostevAA <55791524+RadostevAA@users.noreply.github.com>
2019-12-04 21:01:31 +0300

    Выполнение 1 задания
    Выполнено

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from multiprocessing import Pool
 
 def element(index, A, B):
     i, j = index
@@ -12,5 +12,18 @@ def element(index, A, B):
 
 matrix1 = [[1, 2], [3, 4]]
 matrix2 = [[2, 0], [1, 2]]
+matrix = []
 
-print(element((1, 0), matrix1, matrix2))
+
+if __name__ == '__main__':
+    for i in range(len(matrix1)):
+        a = []
+        for j in range(len(matrix2[0])):
+            b = Pool().starmap(element, [[(i, j), matrix1, matrix2]])[0]
+            a.append(b)
+        matrix.append(a)
+    Pool().close()
+    
+print("Матрица")
+for i in range(len(matrix)):
+    print(matrix[i])
