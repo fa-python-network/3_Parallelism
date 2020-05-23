9d86ce5ef5fe64f4e5fe564ca1fa1b31fd719181
Ksenia <test@te.ru>
2020-05-23 19:34:22 +0300

    test ksenia

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from multiprocessing import Pool
+from random import randint
+
+def matrix_mult(x, y):
+
+	s = sum(i*j for i, j in zip(x,y))
+     
+	return s
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+if __name__ =='__main__':
 
+	matrix1=[[randint(-20, 20) for i in range(3)] for j in range(4)]
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+	matrix2=[[randint(-20, 20) for i in range(4)] for j in range(3)]
 
-print(element((1, 0), matrix1, matrix2))
+	with Pool(2) as pool:
+		mat = pool.starmap(matrix_mult, [(i,j) for i in matrix1 for j in zip(*matrix2)])
+	print(mat)
+
+	result = [mat[i:i+len(matrix2[0])] for i in range(0, len(mat), len(matrix2[0]))]
+	print("Произведение исходных матриц = ", result) 
