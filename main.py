0cc0417daa38aa3a1489c8bf6b2a9b6edd9e593e
Valeria Ermakova <jamie.lie@mail.ru>
2020-05-26 18:49:02 +0300

    Лабораторная работа №3

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
+from multiprocessing import Process, Pool
+
+A = []
+B = []
+project_path = 'C:/Users/jamie/Projects/3_Parallelism/'
+
+def open_file(array, path):
+    with open(path, 'r') as filehandle:
+        for line in filehandle:
+            if '\n' in line:
+                line = line[:-1]
+            array.append(line.split(','))
 
 
-def element(index, A, B):
+def write_file(array, path):
+    with open(path, "w") as txt_file:
+        for item in array:
+            txt_file.write("{} ".format(str(item)) + "\n")
+
+
+open_file(A, project_path + 'Array A.txt')
+open_file(B, project_path + 'Array B.txt')
+
+ai, aj, bi, bj = len(A), len(A[0]), len(B), len(B[0])
+C = [[0 for j in range(bj)] for i in range(ai)]
+
+
+def calculate(index):
     i, j = index
     res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
+    for k in range(len(A[0])):
         res += A[i][k] * B[k][j]
-    return res
+    C[i][j] = int(res)
+    return C
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+if aj != bi:
+    print("Умножение невозможно")
+    exit(1)
 
-print(element((1, 0), matrix1, matrix2))
+for i in range(ai):
+    for j in range(aj):
+        A[i][j] = int(A[i][j])
+
+for i in range(bi):
+    for j in range(bj):
+        B[i][j] = int(B[i][j])
+
+if __name__ == '__main__':
+    pool = Pool(4)
+    result = pool.map(calculate, [(0, 0), (0, 1), (1, 0), (1, 1)])
+    write_file(result[ai + 1], project_path + "Result.txt")
