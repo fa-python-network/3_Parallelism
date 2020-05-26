d3e7948e61c55f9a00dda50e9574689433038c71
Anna <govorova.anna97@mail.ru>
2020-05-26 12:46:06 +0300

    New commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from multiprocessing import Pool
 
-
-def element(index, A, B):
+def element(index):
+    global A
+    global B    
     i, j = index
     res = 0
     # get a middle dimension
@@ -9,8 +11,40 @@ def element(index, A, B):
         res += A[i][k] * B[k][j]
     return res
 
+def lenProc(m1, m2) -> int:
+    x = len(m1[0])
+    y = len(m2)
+    return x*y
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+def matrix1() -> list:
+    result = []
+    with open("matrix1", "r") as f:
+        [result.append(list(map(int, i.split()))) for i in f]  
+    return result
 
-print(element((1, 0), matrix1, matrix2))
+def matrix2() -> list:
+    result = []
+    with open("matrix2", "r") as f:
+        [result.append(list(map(int, i.split()))) for i in f]
+    return result
+
+def writeToFile(result):
+    open("result_matrix", "w").close()
+    with open("result_matrix", "a") as f:
+        [print(" ".join(str(x) for x in line), file = f) for line in result]
+
+A = matrix1()
+B = matrix2()
+print(A)
+print(B)
+pool = Pool(processes = lenProc(A, B))
+
+k = pool.map(element, [(i, j) for i in range(len(A[0])) for j in range(len(B))])
+result = []
+for i in range(len(A[0])):
+    result.append([])
+    for j in range(len(B)):
+        result[-1].append(k[0])
+        k.remove(k[0])
+
+writeToFile(result)
