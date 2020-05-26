2b9ccda46344a7b3cb75eb97b0f424735b7dd04a
NikitaXGod <65556473+NikitaXGod@users.noreply.github.com>
2020-05-26 21:18:28 +0300

    Перемножение матриц

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 def element(index, A, B):
     i, j = index
     res = 0
@@ -14,3 +12,31 @@ matrix1 = [[1, 2], [3, 4]]
 matrix2 = [[2, 0], [1, 2]]
 
 print(element((1, 0), matrix1, matrix2))
+
+
+import numpy as np  
+ 
+a = np.random.randint(0, 100, (2, 2, 2))
+b = np.random.randint(0, 100, (2, 2, 2))
+print('a==', a)
+print('b==', b)
+
+from itertools import starmap
+from operator import mul
+ 
+A = a
+B = b
+ 
+def matrixmul(A, B):
+ 
+    if len(B) != len(A[0]):
+        print("Облом!")
+        return
+ 
+    tmp = tuple(zip(*B))
+ 
+    for row in A:
+        yield [sum(starmap(mul, zip(row, column))) for column in tmp]
+ 
+if "__main__" == __name__:
+    print(list(matrixmul(A, B)))
