423af4b352629e8c0a764aee3891ed524f6ec639
Aramanch <90024366+Aramanch@users.noreply.github.com>
2021-11-18 17:39:17 +0300

    Update ref.py

--- ref.py
+++ ref.py
@@ -1,40 +1,56 @@
+global v
+global n
+
 with open("matrs.txt", "r") as f:
 
     matrix1 = f.readline().strip()
     matrix2 = f.readline().strip()
 
+
+
 f.close()
 
 
-spisok = []
-spisok_1 = []
-spisok_2 = []
-spisok_3 = []
-spisok_4 = []
-spp = []
-
-
+prom_1 = []
+prom_2 = []
+d = []
+v = 0
 for i in matrix1:
-    if i.isdigit() == True:
-        if len(spisok_1) < 2:
-            spisok_1.append(int(i))
-        else:
-            spisok_2.append(int(i))
+    if i == '[':
+        v += 1
 
-spisok.append(spisok_1)
-spisok.append(spisok_2)
+n = v-1
 
-for i in matrix2:
-    if i.isdigit() == True:
-        if len(spisok_3) < 2:
-            spisok_3.append(int(i))
-        else:
-            spisok_4.append(int(i))
+for x in matrix1:
+    if x.isdigit() == True:
+        d.append(int(x))
 
-spp.append(spisok_3)
-spp.append(spisok_4)
+for i in range(0, len(d), n):
+    prom_1.append(d[i:i + n])
 
-mat1 = spisok
-mat2 = spp
+d = []
+
+for y in matrix2:
+    if y.isdigit() == True:
+        d.append(int(y))
+
+for i in range(0, len(d), n):
+    prom_2.append(d[i:i + n])
+
+
+
+
+
+mat1 = prom_1
+mat2 = prom_2
+
+
+
+
+
+
+
+
+
 
 
